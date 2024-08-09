eb6d7424994f16b66aba4eec0e9a32c06bbdbd63
Benjamin Cichos <64483654+BenCichos@users.noreply.github.com>
2024-08-09 16:00:16 +0200

    Update and rename Refractive.jl to Refraction.jl

--- src/Refraction.jl
+++ src/Refraction.jl
@@ -1,4 +1,4 @@
-module Refractive
+module Refraction
 using JLD2
 using BasicInterpolators: LinearInterpolator, NoBoundaries
 using ZipFile
@@ -13,15 +13,14 @@ const RI_DATABASE_PATH = pkgdir(@__MODULE__, "database")
 const RI_DATABASE_DOWNLOAD_PATH = pkgdir(@__MODULE__, "database.zip")
 
 include("materialdata.jl")
-
 include("material.jl")
-export Material, findmaterial, NULL_MATERIAL, isnullmaterial, dispersion
-
 include("update_cache.jl")
+
+export Material, findmaterial, NULL_MATERIAL, isnullmaterial, dispersion
 
 function __init__()
     (ispath(RI_DATA_PATH) && ispath(RI_LIBRARY_PATH)) && return
-    @info "The local cache does not exist. Commencing initialisation of local cache..."
+    @info "The local cache does not exist. Commencing creation of local cache..."
     update_cache("https://refractiveindex.info/download/database/rii-database-2023-10-04.zip")
     return
 end
